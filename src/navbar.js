87fc3d7f7af5db5d1dd5375df526e8d240236d76
Tanya1427 <132272991+Tanya1427@users.noreply.github.com>
2025-04-04 16:29:52 -0500

    I created a navigation bar.

---
+++ src/navbar.js
@@ -0,0 +1,12 @@
+const NavBar = () => {
+    return (
+        <nav className="navbar">
+            <li><a href="">ABOUT ME</a></li>
+            <li><a href="">SKILLS</a></li>
+            <li><a href="">PROJECTS</a></li>
+            <li><a href="">CONTACT ME</a></li>
+        </nav>
+    );
+}
+ 
+export default NavBar;
