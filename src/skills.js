0dbd958ddd555cbe71d042035f413971afe65727
Tanya1427 <132272991+Tanya1427@users.noreply.github.com>
2025-04-05 13:51:17 -0500

    I created a javascript file for the skills section.

---
+++ src/skills.js
@@ -0,0 +1,23 @@
+const Skills = () => {
+    return (
+        <div>
+            <h1 className = "skillsheading" >SKILLS</h1>
+            <div className="container2">
+                <div className="skillbuttons">
+                    <p>Python</p>
+                    <p>HTML/CSS</p>
+                    <p>Javascript</p>
+                    <p>Bootstrap</p>
+                    <p>Git</p>
+                    <p>Github</p> 
+                    <p>Pandas</p>
+                    <p>Numpy</p>
+                    <p>Leadership</p>
+                    <p>Pitching</p>                
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Skills;
